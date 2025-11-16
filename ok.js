ca4404b34c4c015c8cbe569a33740702c1a16003
Vaibhav Deshmukh <dtechofskill@gmail.com>
2025-11-16 14:37:32 +0530

    initial commit: add basic JavaScript functions for greeting and addition

---
+++ ok.js
@@ -0,0 +1,12 @@
+// A simple JavaScript example
+
+function greet(name) {
+  console.log(`Hello, ${name}!`);
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+greet("Alex");
+console.log("2 + 3 =", add(2, 3));
